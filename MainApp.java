0bf85cfb4a124b2d1b0d31f1d7faa67d45386916
TheoDube <theokaelodube1@gmail.com>
2026-05-12 20:59:37 +0200

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -1,22 +1,15 @@
 package com.ub.csi142.app;
 
-import com.ub.csi142.payment.*;
+import com.ub.csi142.vehicles.ElectricCar;
 
 public class MainApp {
-
     public static void main(String[] args) {
-
-        PaymentMethod[] payments = {
-                new CashPayment(),
-                new MobileWalletPayment("wallet123", 500.0)
-        };
-
-        for (PaymentMethod payment : payments) {
-            System.out.println("\nPayment Method: " + payment.getName());
-            payment.pay(100.0);
-        }
-
-        System.out.println("\nTesting payment above wallet limit:");
-        payments[1].pay(700.0);
+        ElectricCar tesla = new ElectricCar("Tesla", "Model 3", 2023, 75);
+        ElectricCar nissan = new ElectricCar("Nissan", "Leaf", 2022, 60);
+        
+        System.out.println("\n--- Describing cars ---");
+        tesla.describe();
+        System.out.println();
+        nissan.describe();
     }
 }
